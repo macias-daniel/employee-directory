fbcd44cb4c3653714770caa15781465463c356a0
PinkFished <d.macias.apps@gmail.com>
2020-05-06 20:35:59 -0700

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Wrapper from "./components/wrapper/Wrapper";
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
-  return (<Wrapper> </Wrapper>)
+  return (
+    <Wrapper>
+      <Navbar />
+    </Wrapper>)
 }
 
 export default App;
--- src/components/wrapper/Wrapper.js
+++ src/components/wrapper/Wrapper.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-function Wrapper() {
-  return <div className="wrapper"></div>
+function Wrapper(props) {
+  return <div className="wrapper">{props.children}</div>
 }
 
 export default Wrapper
