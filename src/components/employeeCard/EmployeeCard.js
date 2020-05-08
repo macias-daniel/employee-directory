2b66f2a06f52a99f8c4c59153474093440904d5e
PinkFished <d.macias.apps@gmail.com>
2020-05-07 18:10:19 -0700

    Added employee card borders

--- src/components/employeeCard/EmployeeCard.js
+++ src/components/employeeCard/EmployeeCard.js
@@ -1,28 +1,26 @@
 import React from "react"
 import "./employeeCard.css"
-import Container from "../layout/Container"
-import Col from "../layout/Col"
-import Row from "../layout/Row"
+
 
 function employeeCard({ firstName, lastName, imgSrc, phoneNum, email }) {
 
   return (
-    <Container>
-      <Row>
-        <Col size="md-3">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 profileImgContainer">
           <img src={imgSrc} className="card-img-top profileImg" alt="Profile" />
-        </Col>
-        <Col size="md-3">
-          <h4 className="name">{`${firstName} ${lastName}`}</h4>
-        </Col>
-        <Col size="md-3">
-          <p className="email">{email}</p>
-        </Col>
-        <Col size="md-3">
-          <p className="phoneNum">{phoneNum}</p>
-        </Col>
-      </Row>
-    </Container >
+        </div>
+        <div className="col-md-3 name">
+          <h4 >{`${firstName} ${lastName}`}</h4>
+        </div>
+        <div className="col-md-3 email">
+          <p >{email}</p>
+        </div>
+        <div className="col-md-3 phoneNum">
+          <p >{phoneNum}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
