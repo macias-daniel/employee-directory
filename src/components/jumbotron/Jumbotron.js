a3b595812d4fbc69559c85f8fe4b5b9a9321d5b9
PinkFished <d.macias.apps@gmail.com>
2020-05-08 15:20:46 -0700

    Added jumbotron caption

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -2,10 +2,10 @@ import React from "react"
 import "./jumbotron.css"
 function Jumbotron() {
   return (
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4 mainText">Keeping track of the humanoids you <em><strong>Need</strong></em> </h1>
-        <p class="lead mt-5">Use our newly created Expendable Humanoid Tracker to deal with all the annoying things that went into tracking your expendables! Search for the humanoid you want in our patent pending searchbar. Even sort humanoids in alphabetical order!  (It's crazy I know)</p>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container textContainer">
+        <h1 className="display-4 mainText">Keeping track of the humanoids you <em><strong>Need</strong></em> </h1>
+        <p className="lead mt-5">Use our newly created Expendable Humanoid Tracker to deal with all the annoying things that went into tracking your expendables! Search for the humanoid you want in our patent pending searchbar. Even sort humanoids in alphabetical order!  (It's crazy I know)</p>
       </div>
     </div >)
 }
